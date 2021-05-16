5c642e825969f56895a2befabaa93f9cf0b2e3c5
RuDomitori <RuDomitori@gmail.com>
2021-05-16 19:51:28 +0700

    Setup dev environment

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Name: <input v-model="name" type="text">
+    <hello-component :name="name" :initialEnthusiasm="5" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloComponent from './components/Hello.vue';
+
+export default Vue.extend({
+  data : { name: "World" },
+  components: {
+    HelloComponent
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// import Vue from "vue";
+import App from "./app.vue";
+
+let vApp = new App({
+    el: "#app",
+    data: { name: "World"}
+});
+
